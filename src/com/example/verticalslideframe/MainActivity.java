7c7f520f5505c836e055117cc4b76147f8d74743
xudonggithub <baobaoc@126.com>
2015-07-03 17:43:43 +0800

    add

---
+++ src/com/example/verticalslideframe/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.verticalslideframe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		ListView listView = (ListView)findViewById(R.id.listview);
+		listView.setAdapter(new BaseAdapter() {
+			
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				TextView textView = new TextView(MainActivity.this);
+				textView.setTextSize(24);
+				textView.setText("text text text");
+				return textView;
+			}
+			
+			@Override
+			public long getItemId(int position) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public Object getItem(int position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				return 20;
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ src/com/example/verticalslideframe/VerticalSlideLayout.java
@@ -0,0 +1,297 @@
+package com.example.verticalslideframe;
+
+
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.R.integer;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.MarginLayoutParams;
+
+
+public class VerticalSlideLayout extends ViewGroup {
+	enum ScrollerLocation {
+		Top, Bottom, Other
+	};
+	private ViewDragHelper mDragHelper;
+	private View topView, bottomView;
+	private int topViewTop;
+	private int topViewTopBound, topViewBottomBound;
+	private boolean initedLayoutParam =false;
+	private ScrollerLocation location = ScrollerLocation.Other;
+	
+	public VerticalSlideLayout(Context context) {
+		this(context, null);
+	}
+	public VerticalSlideLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+	public VerticalSlideLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		mDragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
+		
+	}
+    @Override
+    protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
+        return new VerticalSlideLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new VerticalSlideLayoutParams(p);
+    }
+
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new VerticalSlideLayoutParams(getContext(), attrs);
+    }
+    
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof VerticalSlideLayoutParams && super.checkLayoutParams(p);
+    }
+    
+	@Override
+	protected void onFinishInflate() {
+		topView = findViewById(R.id.top_view);
+		bottomView = findViewById(R.id.bottom_view);
+//		mDragHelper.captureChildView(topView, 0);
+	}
+	
+	@Override
+	public void computeScroll() {
+		if (mDragHelper.continueSettling(true)) {
+			ViewCompat.postInvalidateOnAnimation(this);
+		}
+	}
+	
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+		int childCount = getChildCount();
+		float maxWeight = 0;
+		if (!initedLayoutParam) {
+			float sumWeight = 0;
+			for (int i = 0; i < childCount; i++) {
+				View childView = getChildAt(i);
+				VerticalSlideLayoutParams lp = (VerticalSlideLayoutParams) childView
+						.getLayoutParams();
+				sumWeight += lp.weight;
+				if (maxWeight < lp.weight)
+					maxWeight = lp.weight;
+			}
+			if (sumWeight > 0) {
+				if (heightMode != MeasureSpec.EXACTLY)
+					throw new IllegalStateException(
+							"Height must be MATCH_PARENT for layout_weight.");
+				for (int i = 0; i < childCount; i++) {
+					View childView = getChildAt(i);
+					VerticalSlideLayoutParams lp = (VerticalSlideLayoutParams) childView
+							.getLayoutParams();
+					float weight = lp.weight;
+					int childHeight = (int) (heightSize * (maxWeight / sumWeight));// ignore  margins
+					if (childView == topView) {
+						topViewTopBound = (int) (heightSize * (weight / sumWeight));
+						topViewBottomBound = (int) childHeight;
+						topViewTop = childHeight;
+					}
+					int childHeightSpec = MeasureSpec.makeMeasureSpec(
+							childHeight, MeasureSpec.EXACTLY);
+					int childWidthSpec;
+					if (lp.width == LayoutParams.WRAP_CONTENT) {
+						childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize,
+								MeasureSpec.AT_MOST);
+					} else if (lp.width == LayoutParams.MATCH_PARENT) {
+						childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize,
+								MeasureSpec.EXACTLY);
+					} else {
+						childWidthSpec = MeasureSpec.makeMeasureSpec(lp.width,
+								MeasureSpec.EXACTLY);
+					}
+					childView.measure(childWidthSpec, childHeightSpec);
+				}
+			} else
+				measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+			initedLayoutParam = true;
+		}else {
+			for (int i = 0; i < childCount; i++) {
+				View childView = getChildAt(i);
+				VerticalSlideLayoutParams lp = (VerticalSlideLayoutParams) childView.getLayoutParams();
+				int childHeightSpec = 0, childWidthSpec = 0;
+				if(childView == bottomView) {
+					childHeightSpec = MeasureSpec.makeMeasureSpec(
+							 topViewTop, MeasureSpec.EXACTLY);
+				}
+				else if(childView == topView) {
+					childHeightSpec = MeasureSpec.makeMeasureSpec(
+							heightSize - topViewTop, MeasureSpec.EXACTLY);
+				}
+				if (lp.width == LayoutParams.WRAP_CONTENT) {
+					childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize,
+							MeasureSpec.AT_MOST);
+				} else if (lp.width == LayoutParams.MATCH_PARENT) {
+					childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize,
+							MeasureSpec.EXACTLY);
+				} else {
+					childWidthSpec = MeasureSpec.makeMeasureSpec(lp.width,
+							MeasureSpec.EXACTLY);
+				}
+				childView.measure(childWidthSpec, childHeightSpec);
+				
+			}
+		}
+		
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+	
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		int childCount = getChildCount();
+//		int childTop = 0;
+		for (int i = 0; i < childCount; i++) {
+			View childView = getChildAt(i);
+			final int childWidth = childView.getMeasuredWidth();
+            final int childHeight = childView.getMeasuredHeight();
+			MarginLayoutParams lp = (MarginLayoutParams)childView.getLayoutParams();
+			if(childView == bottomView) {
+				childView.layout(l, t, l + childWidth, t + childHeight);
+			}
+			else if(childView == topView) {
+				childView.layout(l, topViewTop, l + childWidth, topViewTop + childHeight);
+			}
+		}
+	}
+	
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mDragHelper.cancel();
+            return false;
+        }
+        boolean ret = mDragHelper.shouldInterceptTouchEvent(ev);
+        return ret || checkTouchEventEccepted(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+//    	if(checkTouchEventEccepted(ev))
+    		mDragHelper.processTouchEvent(ev);
+        return true;
+    }
+
+    private boolean checkTouchEventEccepted(MotionEvent ev) {
+    	 boolean isUnderTop = mDragHelper.isViewUnder(topView, (int) ev.getX(), (int) ev.getY());
+         boolean isTopCanMoved = isUnderTop && location != ScrollerLocation.Top;
+         boolean isUnderBottom = mDragHelper.isViewUnder(bottomView, (int) ev.getX(), (int) ev.getY());
+         boolean isBottomCanMoved = isUnderBottom && location != ScrollerLocation.Bottom;
+         System.out.println("cxd,isTopCanMoved="+isTopCanMoved+",isBottomCanMoved="+isBottomCanMoved+",location="+location);
+         return isTopCanMoved || isBottomCanMoved;
+    }
+	
+	private class DragHelperCallback extends ViewDragHelper.Callback {
+
+		@Override
+		public boolean tryCaptureView(View arg0, int arg1) {
+			return arg0 == topView || arg0 == bottomView;
+		}
+		
+		@Override
+		public int clampViewPositionVertical(View child, int top, int dy) {
+			int newTop = 0;
+			if (child == topView) {
+				newTop = Math.min(Math.max(top, topViewTopBound), topViewBottomBound);
+				
+			} else if (child == bottomView) {
+				topViewTop += dy;
+				newTop = Math.min(Math.max(topViewTop, topViewTopBound), topViewBottomBound);
+				topViewTop = newTop;
+			}
+			
+			return newTop;
+		}
+
+		@Override
+		public void onViewPositionChanged(View changedView, int left, int top,
+				int dx, int dy) {
+			if(changedView == topView)
+				topViewTop = top;
+			if (top == topViewBottomBound){
+				location = ScrollerLocation.Bottom;
+				System.out.println("cxd, location set to Bottom");
+			}
+			else if (top == topViewTopBound) {
+				location = ScrollerLocation.Top;
+				System.out.println("cxd, location set to Top");
+			}
+			
+			requestLayout();
+		}
+
+		@Override
+		public void onViewReleased(View releasedChild, float xvel, float yvel) {
+			if(releasedChild == bottomView)
+			{
+				if(yvel > 0)
+					mDragHelper.smoothSlideViewTo(topView, topView.getLeft(), topViewBottomBound);
+				else if(yvel < 0)
+					mDragHelper.smoothSlideViewTo(topView, topView.getLeft(), topViewTopBound);
+			}
+			else if(releasedChild == topView) {
+	 			if(yvel > 0) 
+					mDragHelper.settleCapturedViewAt(releasedChild.getLeft(), topViewBottomBound);
+				else if(yvel < 0)
+					mDragHelper.settleCapturedViewAt(releasedChild.getLeft(), topViewTopBound);
+				}
+			invalidate();
+		}
+	}
+	
+	public static class VerticalSlideLayoutParams extends MarginLayoutParams {
+
+		private static final int[] ATTRS = new int[] { android.R.attr.layout_weight };
+
+		private float weight = 0;
+
+		public VerticalSlideLayoutParams(Context context, AttributeSet attrs) {
+			super(context, attrs);
+			final TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
+			this.weight = a.getFloat(0, 0);
+			a.recycle();
+		}
+
+		public VerticalSlideLayoutParams(VerticalSlideLayoutParams source) {
+			super(source);
+			this.weight = source.weight;
+		}
+
+		public VerticalSlideLayoutParams(MarginLayoutParams source) {
+			super(source);
+		}
+
+		public VerticalSlideLayoutParams(ViewGroup.LayoutParams source) {
+			super(source);
+		}
+
+		public VerticalSlideLayoutParams(int width, int height) {
+			super(width, height);
+		}
+
+	}
+}
